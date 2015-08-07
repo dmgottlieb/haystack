0794366404e738de7d7fc65804cd4b0359fda9fa
David Gottlieb <dmgottlieb@gmail.com>
2015-08-07 18:25:59 -0400

    Logger -- a few more details

--- Logger.lua
+++ Logger.lua
@@ -2,24 +2,30 @@
 -- Idea: collect logs of sheep locations (and other info?) from game testing, for analysis. 
 
 Logger = {
-	events = {}
+	events = {},
+	header = "Haystack tactics log\n",
+	data_fields = "event, x, y, time, memo" -- csv line with variable names
 }
 
 function Logger:new()
 	o = {}
 	setmetatable(o, self)
 	self.__index = self
-	
+
+
+	-- Idea: initialize log header with automatic version info from git hook
 	return o
 end
 
 function Logger:addEvent()
-
+	-- Add csv line describing event
 end
 
 function Logger:writeLog()
-	name = "" .. os.time()
+	name = "logs/hslog-" .. os.time() .. ".csv"
 	f = love.filesystem.newFile(name)
+	f:open("w")
+	f:write(self.header)
 	for i,l in ipairs(self.events) do
 		f.write(l)
 	end
