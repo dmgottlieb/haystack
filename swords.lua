191bc22cbabd392a38578de67c34c92c872434fe
David Gottlieb <dmgottlieb@gmail.com>
2015-08-12 16:37:20 -0400

    Hand-drawn sword animation (doesn't match collision logic)

--- swords.lua
+++ swords.lua
@@ -9,13 +9,13 @@ function DrawSword(s)
 	r,g,b = s.player.color[1], s.player.color[2], s.player.color[3]
 	love.graphics.setColor(r,g,b)
 
-	-- frame = math.ceil(9 * s.theta / 360)
-	-- local img  = love.graphics.newImage("assets/swordsheet.png")
-	-- local quad = love.graphics.newQuad((frame-1)*78, 0, 78, 55, img:getDimensions())
-	-- love.graphics.draw(img, quad, hilt.x, hilt.y, math.rad(s.player.direction),1,1, 26, 15)
+	frame = math.ceil(9 * (s.theta+30) / 60)
+	local img  = love.graphics.newImage("assets/swordsheet.png")
+	local quad = love.graphics.newQuad((frame-1)*78, 0, 78, 55, img:getDimensions())
+	love.graphics.draw(img, quad, s.player.position.x, s.player.position.y, math.rad(s.player.direction),1,1,26,30)
 
-	love.graphics.setLineWidth(5)
-	love.graphics.line(hilt.x, hilt.y, tip.x, tip.y)
+	-- love.graphics.setLineWidth(5)
+	-- love.graphics.line(hilt.x, hilt.y, tip.x, tip.y)
 end
 
 function DrawSwords(S)
